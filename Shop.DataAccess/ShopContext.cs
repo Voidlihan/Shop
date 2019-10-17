5b3d497a4875ef45ccb65d1dd65f2178e634052b
e.alihangt@gmail.com <e.alihangt@gmail.com>
2019-10-17 20:04:44 +0600

    shop

---
+++ Shop.DataAccess/ShopContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Shop.Domain;
+
+namespace Shop.DataAccess
+{
+    public class ShopContext : DbContext
+    {
+        private readonly string connectionString;
+
+        public ShopContext(string connectionString)
+        {
+            this.connectionString = connectionString;
+            Database.EnsureCreated();
+        }
+
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //optionsBuilder.UseSqlServer(connectionString);
+            optionsBuilder.UseInMemoryDatabase("ShopDB");
+        }
+    }
+}
---
+++ Shop.ui/Program.cs
@@ -0,0 +1,52 @@
+﻿using Shop.Domain;
+using Shop.DataAccess;
+using System;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+using System.Linq;
+using System.Data.Common;
+using System.Data.SqlClient;
+
+/*
+ *  1.Регистрация и вход (смс/email)
+ *  2.История покупок
+ *  3.Категории и товары (картинка в файловой системе)
+ *  4.Покупка(корзина), оплата и доставка (PayPal, Qiwi, ETC)
+ *  5.Комментарии и рейтинги
+ *  6.Поиск (пагинация)
+ *  
+ *  Кто сделает 3 версии(Подключенный, автономный и EF) получит автомат на экзамене
+ */
+
+namespace Shop.ui
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", false, true);
+            IConfigurationRoot configurationRoot = builder.Build();
+            string providerName = configurationRoot.GetSection("AppConfig").
+                GetChildren().
+                Single().Value;
+            DbProviderFactories.RegisterFactory(providerName, SqlClientFactory.Instance);
+            Category category = new Category
+            {
+                Name = "Бытовая техника",
+                ImagePath = "C:/data"
+            };
+            
+            string connectionString = configurationRoot.GetConnectionString("DebugConnectionString");
+            using (var context = new ShopContext(connectionString))
+            {
+                context.Categories.Add(category);
+                var res = context.Categories.ToList();
+                context.Categories.Remove(category);
+                context.Remove(category);
+                context.SaveChanges();
+            }
+        }
+    }
+}
